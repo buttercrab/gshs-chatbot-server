e78c6c31da7ae97bef67ea03af4d404dd98d51fc
buttercrab <jaeyong0201@gmail.com>
2019-07-21 17:47:05 +0900

    managed full api functions

--- src/apiHandler/apiHandler.go
+++ src/apiHandler/apiHandler.go
@@ -2,8 +2,8 @@ package apiHandler
 
 import (
 	"encoding/json"
-	"errors"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -21,50 +21,90 @@ type UserData struct {
 	Etc      string `json:"etc"`
 }
 
-func IsLoggedIn(id string) (bool, *UserData) {
+type ApiResponse struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
+type ApiSearchRequest struct {
+	Code    string        `json:"code"`
+	Message string        `json:"message"`
+	Size    int           `json:"size"`
+	List    []GoodsInform `json:"list"`
+}
+
+type GoodsInform struct {
+	Site       string `json:"site"`
+	StartDate  string `json:"startDate"`
+	EndDate    string `json:"endDate"`
+	Accept     string `json:"accept"`
+	Teacher2   string `json:"teacher2"`
+	GoodsNo    int    `json:"goodsNo"`
+	ManageNo   string `json:"manageNo"`
+	Content    string `json:"content"`
+	Teacher1   string `json:"teacher1"`
+	Target     string `json:"target"`
+	UserName   string `json:"userName"`
+	GoodsUseNo int    `json:"goodsUseNo"`
+	GoodsName  string `json:"goodsName"`
+}
+
+func GetLoginURL(id string) string {
+	return "http://external.gs.hs.kr/external/regChatBot.do?user_key" + id
+}
+
+func GetUserData(id string) (*ApiResponse, *UserData) {
 	res, _ := http.Get("http://external.gs.hs.kr/external/chatbot/getChatBotUser.do?user_key=" + id)
 	defer res.Body.Close()
 
 	var t ApiUserResponse
 	_ = json.NewDecoder(res.Body).Decode(&t)
 
-	if t.Code == "0000" {
-		return true, &t.List[0]
-	} else {
-		return false, nil
-	}
+	return &ApiResponse{Code: t.Code, Message: t.Message}, &t.List[0]
 }
 
-type ApiResponse struct {
-	Code    string `json:"code"`
-	Message string `json:"message"`
-}
-
-func LaptopApplyRequest(id string, user *UserData) error {
-	goodsNo := ""
-	switch user.Etc {
-	case "1":
-		goodsNo = "271"
-	case "2":
-		goodsNo = "272"
-	case "3":
-		goodsNo = "273"
-	}
-	res, _ := http.Get("http://external.gs.hs.kr/external/chatbot/insertGoodsUse.do" +
-		"?user_key=" + id +
-		"&userType=" + user.UserType +
-		"&userId=" + user.UserId +
-		"&goodsNo=" + goodsNo +
-		"&startDate=" + time.Now().Format("20060102") + "1900" +
-		"&endDate=" + time.Now().Format("20060102") + "2100" +
-		"&useTarget=" + "[%EC%B1%97%EB%B4%87]")
+func ExpireUser(id string) *ApiResponse {
+	res, _ := http.Get("http://external.gs.hs.kr/external/chatbot/expireChatBotUser.do?user_key=" + id)
+	defer res.Body.Close()
 
 	var t ApiResponse
 	_ = json.NewDecoder(res.Body).Decode(&t)
 
-	if t.Code != "0000" {
-		return errors.New(t.Code + " " + t.Message)
-	}
+	return &t
+}
 
-	return nil
+func SearchGoodsUse(id string, goodsNo int) (*ApiResponse, *[]GoodsInform) {
+	res, _ := http.Get("http://external.gs.hs.kr/external/chatbot/goodsUseList.do?user_key=" + id +
+		"&goodsNo=" + strconv.Itoa(goodsNo))
+	defer res.Body.Close()
+
+	var t ApiSearchRequest
+	_ = json.NewDecoder(res.Body).Decode(&t)
+
+	return &ApiResponse{Code: t.Code, Message: t.Message}, &t.List
+}
+
+func RequestGoodsUse(id string, goodsNo int, startDate, endDate time.Time) *ApiResponse {
+	res, _ := http.Get("http://external.gs.hs.kr/external/chatbot/insertGoodsUse.do?user_key=" + id +
+		"&goodsNo=" + strconv.Itoa(goodsNo) +
+		"&startDate=" + startDate.Format("200601021504") +
+		"&endDate=" + endDate.Format("200601021504") +
+		"&useTarget=" + "[%EC%B1%97%EB%B4%87]")
+	defer res.Body.Close()
+
+	var t ApiResponse
+	_ = json.NewDecoder(res.Body).Decode(&t)
+
+	return &t
+}
+
+func CancelGoodsUse(id string, goodsUseNo int) *ApiResponse {
+	res, _ := http.Get("http://external.gs.hs.kr/external/chatbot/deleteGoodsUse.do?user_key=" + id +
+		"&goodsUseNo=" + strconv.Itoa(goodsUseNo))
+	defer res.Body.Close()
+
+	var t ApiResponse
+	_ = json.NewDecoder(res.Body).Decode(&t)
+
+	return &t
 }
