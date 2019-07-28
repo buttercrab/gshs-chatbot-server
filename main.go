6f0bd52748ce7fc607af98f8301771c196483de2
buttercrab <jaeyong0201@gmail.com>
2019-07-29 01:32:16 +0900

    changed port

--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"github.com/buttercrab/gshs-chatbot-server/httpHandler"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
 	http.HandleFunc("/api/request", httpHandler.RequestHandler)
 	http.HandleFunc("/api/cancel", httpHandler.CancelHandler)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
